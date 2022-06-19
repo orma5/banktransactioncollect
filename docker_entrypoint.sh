afbdcb415afcc9070efdea52bff5f61f64a4cc6e
Ploppe <po.markstrom@gmail.com>
2022-06-19 20:52:34 +0200

    ..and again

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
-chmod +x /root/project_env.sh
+printenv | sed 's/^\(.*\)$/export \1/g' > /banktransactioncollect/project_env.sh
+chmod +x /banktransactioncollect/project_env.sh
 cron -f
